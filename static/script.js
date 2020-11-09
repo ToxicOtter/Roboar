d2e1558c042af3c6af4bbee69bcda93243c60f83
ToxicOtter <luan.lima.lopes16@gmail.com>
2020-11-09 12:00:07 -0300

    Site style
    Added a carousel sldier and the firts version of data viwer, presented in a grid 1x2. Was added a favicon and a new logo.

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-function ajax(){
+/*function ajax(){
     $.ajax ({
         url: '/_get_data/',
         type: 'POST',
@@ -6,4 +6,19 @@ function ajax(){
             $('div#response').append(resp.data);
         }
     })
-}
+}*/
+
+
+/* ----------- atualize div ------------------- */
+$(function() {
+    setTime();
+    function setTime() {
+       var date = new Date().getTime();
+       /*var string = "Timestamp: "+date;*/
+       var string = Math.floor(Math.random() * 10);
+       var otherstring = Math.floor(Math.random() * 10);
+       setTimeout(setTime, 3000);
+       $('#data1').html(string);
+       $('#data2').html(otherstring);
+    }
+  });

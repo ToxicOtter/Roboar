875ee160ace564e80dfc8ff647712832e41e60af
ToxicOtter <luan.lima.lopes16@gmail.com>
2020-11-17 11:37:00 -0300

    Site extruturation complete
    Design and extruturation of the contents are complete. Random images were apply only to cover the spaces, in new features we'll add the final images.

---
+++ static/js/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    // atualize div
+    $(function() {
+        setTime();
+        function setTime() {
+            var date = new Date().getTime();
+            /*var string = "Timestamp: "+date;*/
+            var string = Math.floor(Math.random() * 10);
+            var otherstring = Math.floor(Math.random() * 10);
+            setTimeout(setTime, 3000);
+            $('#data1').html(string);
+            $('#data2').html(otherstring);
+        }
+    });
+
+//animated scroll
+    //buttons
+    let navBtn = $('.nav-item');
+    let mainBtn = $('.main-btn');
+
+    //areas
+    let slider = $('#mainSlider');
+    let data = $('#data-area');
+    let about = $('#about-area');
+    let team = $('#team-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+        //"ifs" to redirect to the area
+        let btnId = $(this).attr('id');
+
+        if (btnId == 'data-menu'){
+            scrollTo = data;
+        } else if (btnId == 'about-menu'){
+            scrollTo = about;
+        } else if (btnId == 'team-menu'){
+            scrollTo = team;
+        } else if (btnId == 'home-menu'){
+            scrollTo = slider;
+        }
+
+        //animation
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500)
+    });
+
+    $(mainBtn).click(function() {
+        //same explanation of the above
+        let mainId = $(this).attr('id');
+
+        if (mainId == 'data-slide'){
+            scrollTo = data;
+        } else if (mainId == 'project-slide'){
+            scrollTo = about;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1000)
+    });
+
+});
+/*function ajax(){
+    $.ajax ({
+        url: '/_get_data/',
+        type: 'POST',
+        success: function(resp){
+            $('div#response').append(resp.data);
+        }
+    })
+}*/

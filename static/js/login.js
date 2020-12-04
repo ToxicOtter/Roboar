fc9621ee64afcc98b99ecd113bbd2455b74c631d
ToxicOtter <luan.lima.lopes16@gmail.com>
2020-12-03 21:47:02 -0300

    Comments and interface updates.

--- static/js/login.js
+++ static/js/login.js
@@ -2,12 +2,14 @@ var x = document.getElementById("login");
 var y = document.getElementById("register");
 var z = document.getElementById("btn");
 
+// move the div itens to register
 function register(){
     x.style.left = "-400px";
     y.style.left = "50px";
     z.style.left = "110px";    
 }
 
+// move the div itens to login
 function login(){
     x.style.left = "50px";
     y.style.left = "450px";
